dcd9bc0faece677e67276187bfbab11656ca11b4
Luis Kao <kogiokkafrms@gmail.com>
2020-09-24 10:46:03 +0800

    Improve VertexArray class by inline and const methods

--- include/VertexArray.hpp
+++ include/VertexArray.hpp
@@ -19,9 +19,33 @@ class VertexArray
 public:
   VertexArray();
   ~VertexArray();
-  GLuint Id();
+  GLuint Id() const;
   void Bind() const;
   bool AddAttribFormat(std::string const& attribName, GLuint index, AttribFormat const& format);
-  void Enable(std::string const& attribName);
-  void Disable(std::string const& attribName);
+  void Enable(std::string const& attribName) const;
+  void Disable(std::string const& attribName) const;
 };
+
+inline void
+VertexArray::Bind() const
+{
+  glBindVertexArray(id_);
+}
+
+inline GLuint
+VertexArray::Id() const
+{
+  return id_;
+}
+
+inline void
+VertexArray::Enable(std::string const& attribName) const
+{
+  glEnableVertexArrayAttrib(id_, attribTable_.at(attribName));
+}
+
+inline void
+VertexArray::Disable(std::string const& attribName) const
+{
+  glDisableVertexArrayAttrib(id_, attribTable_.at(attribName));
+}
--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -10,37 +10,13 @@ VertexArray::~VertexArray()
   glDeleteVertexArrays(1, &id_);
 }
 
-void
-VertexArray::Bind() const
-{
-  glBindVertexArray(id_);
-}
-
-GLuint
-VertexArray::Id()
-{
-  return id_;
-}
-
 bool
 VertexArray::AddAttribFormat(std::string const& attribName, GLuint index, AttribFormat const& format)
 {
   if (attribTable_.find(attribName) != attribTable_.end()) {
     return false;
   }
-  attribTable_.insert({attribName, index});
+  attribTable_.emplace(attribName, index);
   glVertexArrayAttribFormat(id_, index, format.count, format.type, format.normalized, 0);
   return true;
-}
-
-void
-VertexArray::Enable(std::string const& attribName)
-{
-  glEnableVertexArrayAttrib(id_, attribTable_[attribName]);
-}
-
-void
-VertexArray::Disable(std::string const& attribName)
-{
-  glDisableVertexArrayAttrib(id_, attribTable_[attribName]);
 }
