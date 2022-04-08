4cb310566f2ad381d3c99159a522b6f1b6d738cc
Luis Kao <kogiokka.frms@gmail.com>
2022-04-08 13:44:57 +0800

    Clean up

--- include/assetlib/STL/STLImporter.hpp
+++ include/assetlib/STL/STLImporter.hpp
@@ -6,20 +6,6 @@
 #include <string>
 #include <vector>
 
-enum State : int {
-  SOLID = 0,
-  FACET = 1,
-  NORMAL = 1 << 1,
-  OUTER = 1 << 2,
-  LOOP = 1 << 3,
-  VERTEX = 1 << 4,
-  ENDLOOP = 1 << 5,
-  ENDFACET = 1 << 6,
-  ENDSOLID = 1 << 7,
-  READ_NORMAL,
-  READ_VERTEX,
-};
-
 class STLImporter
 {
   std::string name_;
