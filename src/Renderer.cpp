1c6fce7f416b5deef5e6886e3f762ca645ea55a7
Luis Kao <kogiokka.frms@gmail.com>
2022-07-01 10:20:06 +0800

    Remove render flag conditions in the Renderer

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -18,39 +18,25 @@ void Renderer::Render()
     if (!world.theModel) {
         return;
     }
-
-    if (world.renderFlags & RenderFlag_DrawLightSource) {
-        m_lightSource->Update(m_gfx);
-        m_lightSource->Draw(m_gfx);
+    if (world.theModel->HasNormals()) { // FIXME Is this reliable?
+        m_polyModel->Update(m_gfx);
+        m_polyModel->Draw(m_gfx);
+    } else {
+        glEnable(GL_CULL_FACE); // FIXME: VertexArrray bindable?
+        glCullFace(GL_BACK);
+        m_volModel->Update(m_gfx);
+        m_volModel->Draw(m_gfx);
+        glDisable(GL_CULL_FACE);
     }
+    m_lightSource->Update(m_gfx);
+    m_latticeVert->Update(m_gfx);
+    m_latticeEdge->Update(m_gfx);
+    m_latticeFace->Update(m_gfx);
 
-    if (world.renderFlags & RenderFlag_DrawLatticeVertex) {
-        m_latticeVert->Update(m_gfx);
-        m_latticeVert->Draw(m_gfx);
-    }
-
-    if (world.renderFlags & RenderFlag_DrawModel) {
-        if (world.theModel->HasNormals()) { // FIXME Is this reliable?
-            m_polyModel->Update(m_gfx);
-            m_polyModel->Draw(m_gfx);
-        } else {
-            glEnable(GL_CULL_FACE); // FIXME: VertexArrray bindable?
-            glCullFace(GL_BACK);
-            m_volModel->Update(m_gfx);
-            m_volModel->Draw(m_gfx);
-            glDisable(GL_CULL_FACE);
-        }
-    }
-
-    if (world.renderFlags & RenderFlag_DrawLatticeEdge) {
-        m_latticeEdge->Update(m_gfx);
-        m_latticeEdge->Draw(m_gfx);
-    }
-
-    if (world.renderFlags & RenderFlag_DrawLatticeFace) {
-        m_latticeFace->Update(m_gfx);
-        m_latticeFace->Draw(m_gfx);
-    }
+    m_lightSource->Draw(m_gfx);
+    m_latticeVert->Draw(m_gfx);
+    m_latticeEdge->Draw(m_gfx);
+    m_latticeFace->Draw(m_gfx);
 }
 
 void Renderer::LoadPolygonalModel(Mesh const& mesh)
