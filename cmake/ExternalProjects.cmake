6cc4f7946e6ff08ab5c038a7d936514966ef8118
Luis Kao <kogiokka.frms@gmail.com>
2022-11-02 16:07:20 +0800

    Combine find_package with FetchContent

---
+++ cmake/ExternalProjects.cmake
@@ -0,0 +1,27 @@
+set(glm_VERSION 0.9.9)
+set(wxWidgets_VERSION 3.2)
+
+find_package(glm ${glm_VERSION} QUIET)
+find_package(wxWidgets ${wxWidgets_VERSION} COMPONENTS base gl core aui QUIET)
+
+set(EXTERNAL_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external")
+
+include(FetchContent)
+
+if (NOT glm_FOUND)
+    message(STATUS "Could NOT found glm ${glm_VERSION}, populating the bundled one.")
+    include("cmake/FetchGLM.cmake")
+endif()
+
+if (wxWidgets_FOUND)
+    include(${wxWidgets_USE_FILE})
+else()
+    message(STATUS "Could NOT found wxWidgets ${wxWidgets_VERSION}, populating the bundled one.")
+    include("cmake/FetchWx.cmake")
+    set(wxWidgets_LIBRARIES wx::base wx::core wx::gl wx::aui)
+endif()
+
+add_subdirectory("${EXTERNAL_DIR}/librvl-v0.6.4")
+include("cmake/BuildGLAD.cmake")
+include("cmake/BuildStbImage.cmake")
+include("cmake/BuildTomlPlusPlus.cmake")
