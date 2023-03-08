a5d1a409a50b4ad30a1898a9a59241da56acf1a5
Luis Kao <kogiokka.frms@gmail.com>
2023-03-08 15:55:46 +0800

    Remove tomlplusplus dependency

--- cmake/BuildTomlPlusPlus.cmake
+++
@@ -1,8 +0,0 @@
-if (NOT EXTERNAL_DIR)
-    message(FATAL_ERROR "Variable EXTERNAL_DIR is not defined.")
-endif()
-
-add_library(tomlplusplus INTERFACE)
-
-target_include_directories(tomlplusplus INTERFACE "${EXTERNAL_DIR}/tomlplusplus/")
-
